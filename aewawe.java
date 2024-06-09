1bb882a3f269f1197816a2f290528fb56a62ab13
Eunsuk Kim <didier@queelive.co.kr>
2024-06-09 17:43:44 +0900

    awegw

---
+++ aewawe.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;/**
+ * @프로젝트명		: aabb
+ * @클래스명		: aewawe
+ * @패키지 정보  : PACKAGE_NAME
+ * @설명		: 
+ * @작성자 eunsukkim
+ * @작성일		: 9/6/24 5:43 PM
+ * @version v.0.1
+ */public class aewawe {
+}
